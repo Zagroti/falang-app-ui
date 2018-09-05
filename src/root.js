e088cfd21f7a98535d119d7510dbb1b8ab6c3883
aras armani <aras.armani2014@gmail.com>
2018-09-05 15:15:11 +0300

    splash screen added

--- src/root.js
+++ src/root.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
+import { Platform } from 'react-native';
+import SplashAuto from 'react-native-splash-screen';
 
 
 
 //
 //  import Our Component
 //
-
+import SplashScreen from './splash';
 import MainScreen from './components/main';
 import AuthScreen from './components/auth';
 
+
+
+//
+// Main and Maser component
+//
+
 const RootStack = createStackNavigator({
 
-
+    Splash : {screen : SplashScreen},
     Main : {screen : MainScreen},
     Auth : {screen : AuthScreen},
 
@@ -24,13 +32,22 @@ const RootStack = createStackNavigator({
 }) 
 
 
+//
+// export App from root stack
+//
+
 export default class Root  extends React.Component{
+
+    componentDidMount() {
+        if (Platform.OS !== 'ios') SplashAuto.hide();
+        console.disableYellowBox = true
+      }
+
+
     render(){
-
-
         return(
-            <RootStack />
 
+            <RootStack />
         );
     }
 }
