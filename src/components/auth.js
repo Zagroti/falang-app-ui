40de1952f776ae81ee6dcd0b166eb18ae6c7875c
aras armani <aras.armani2014@gmail.com>
2018-09-07 22:08:44 +0300

    TextInput configuration added

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,47 +1,114 @@
 import React, {Component } from "react";
-// import { Container, Left, Button, Body, Title, Right, Header , Icon} from 'native-base';
-
+ 
 import {
      
-    Image,
-    Text,
+    Image, 
     StyleSheet,
-    ImageBackground
+    ImageBackground, 
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    TextInput,
+    Text
 
   } from 'react-native';
+import colors from '../styles/colors';
+ 
 
-const remote = 'https://s15.postimg.org/tw2qkvmcb/400px.png';
-
+ 
 class Auth extends Component{
 
 
+    state = {
+        errors: {},
+        email: '',
+        password: '',
+        isLoading: false,
+      }
   
     render(){
 
-      
-        const resizeMode = 'center';
-        const text = 'This is some text inlaid in an <Image />';
-
         return (
+            <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : -300}
+          >
+                <ImageBackground source={require('../assets/img/climb.png')} style={styles.backgroundImage}>
+                    <ImageBackground source={require('../assets/img/gradient.png')} style={styles.backgroundImage}>
 
-        <ImageBackground source={require('../assets/img/climb.png')} style={{width: '100%', height: '100%'}}>
-            <Text>Inside</Text>
-        </ImageBackground>
+                        <View style={styles.logoContainer}>
+                            
+                            <Image
+                                source={require('../assets/img/logo.png')}
+                                style={styles.logo}
+                            />
+                            
+                        </View>
 
+                        <View style={styles.formContainer}>
+                            <TextInput
+                                style={{height: 40}}
+                                placeholder="Type here to translate!"
+                                onChangeText={(text) => this.setState({text})}
+                                />
+
+                            <TextInput
+                                style={{height: 40}}
+                                placeholder="Type here to translate!"
+                                onChangeText={(text) => this.setState({text})}
+                                />
+
+                        
+
+                        </View>
+                            
+                    </ImageBackground>
+                </ImageBackground>
+            </KeyboardAvoidingView>
            
         );
     }
 }
 
 
+// const { height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+      },
     backgroundImage: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover'
-    }
+        width: '100%',
+        height: '100%',
+
+    },
+    transparentBg:{
+        backgroundColor: "transparent",
+        borderColor: "transparent",
+
+    },
+      logoContainer: {
+        marginTop: -30,
+        minHeight: 190,
+        justifyContent: 'center',
+        alignItems: 'center', 
+      },
+      linearGradient: {
+        paddingTop: 90,
+        paddingBottom: 40, 
+        backgroundColor: colors.themeBackground,
+      },
+      formContainer: {
+        height:  200,
+        paddingHorizontal: 30,
+        paddingTop: 60,
+      },
+      logo: {
+        resizeMode: 'contain',
+        height: 104,
+      },
 });
 
 
