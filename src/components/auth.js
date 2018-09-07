01cf0cfcaa03191cd749edf5620aee4c7be51e2b
aras armani <aras.armani2014@gmail.com>
2018-09-07 16:55:59 +0300

    background image added!

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,24 +1,49 @@
 import React, {Component } from "react";
-import {  View , Text  } from "react-native";
+// import { Container, Left, Button, Body, Title, Right, Header , Icon} from 'native-base';
 
+import {
+     
+    Image,
+    Text,
+    StyleSheet,
+    ImageBackground
 
+  } from 'react-native';
 
+const remote = 'https://s15.postimg.org/tw2qkvmcb/400px.png';
 
 class Auth extends Component{
 
 
-
+  
     render(){
+
+      
+        const resizeMode = 'center';
+        const text = 'This is some text inlaid in an <Image />';
+
         return (
 
-            <View>
-                <Text>
-                    It's Auth page, Hello
-                </Text>
-            </View>
-        )
+        <ImageBackground source={require('../assets/img/climb.png')} style={{width: '100%', height: '100%'}}>
+            <Text>Inside</Text>
+        </ImageBackground>
+
+           
+        );
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover'
+    }
+});
+
 
 
 export default Auth ;
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,7 @@
 import React, {Component } from "react";
 import {  View , Text  } from "react-native";
+import { Container, Left, Button, Body, Title, Right, Header , Icon} from 'native-base';
+
 
 
 
@@ -11,11 +13,23 @@ class Main extends Component{
     render(){
         return (
 
-            <View>
-                <Text>
-                    It's Main page , Hello
-                </Text>
-            </View>
+            <Container>
+
+            <Header style={{backgroundColor: '#34495E', border:0}}>
+                <Left>
+
+                </Left>
+                <Body>
+                <Title>Github...</Title>
+                </Body>
+                    <Right>
+                        <Button transparent>
+                            <Icon name='menu'/>
+                        </Button>
+                    </Right>
+            </Header>
+
+            </Container>
         )
     }
 }
