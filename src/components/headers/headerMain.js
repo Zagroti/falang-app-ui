78b1194bf07f9accec658666758a28fb8c8c3655
aras armani <aras.armani2014@gmail.com>
2018-11-01 11:02:37 +0300

    headers is fixed

--- src/components/headers/headerMain.js
+++ src/components/headers/headerMain.js
@@ -1,19 +1,64 @@
-import React,{Component }  from 'react';
-import  { View, Text  } from 'react-native';
+import React, { Component } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { DrawerNavigator, DrawerItems}  from 'react-navigation';
+import Icon from 'react-native-vector-icons/dist/Feather';
+
+ 
 
 class HeaderMain extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
-    render() { 
-        return ( 
+   
+  onButtonPress = () => {
+    this.props.navigation.openDrawer('DrawerOpen');
 
-            <View>
+  }
 
-            </View>
-         );
-    }
+  render() { 
+ 
+    return ( 
+
+     <View style={styles.container}>
+       
+        <View style={styles.titleContainer}>
+            <Text style={{color:'#fff', fontSize:16}}>{this.props.title}</Text>
+        </View>
+
+        <TouchableOpacity onPress = { this.onButtonPress }  style={styles.menuContainer}>
+            <Icon name="menu" size={24} color="#fff"  />
+        </TouchableOpacity>
+     </View>
+         
+
+      
+     );
+  }
 }
+ 
+
+const styles = StyleSheet.create({
+
+  container:{
+    height:50,
+    backgroundColor:'#E74C3C',
+    flexDirection: 'row',
+
+  },
+  menuContainer:{
+      flex: 1,
+      alignItems:'center',
+      justifyContent:'center',
+
+
+  },
+  titleContainer:{
+    paddingLeft:20,
+    alignItems:'flex-start',
+    justifyContent:'center',
+    flex:5
+  }
+})
  
 export default HeaderMain;
