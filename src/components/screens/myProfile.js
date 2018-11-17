bdd5de64d811252293bf2abd93d3cbe36f8c7e16
aras armani <aras.armani2014@gmail.com>
2018-11-17 15:52:17 +0300

    my profile is fixed

--- src/components/screens/myProfile.js
+++ src/components/screens/myProfile.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, StyleSheet } from 'react-native';
 
-
+//
+//
+import colors from '../../styles/colors'
+import Icon from '../../styles/icons'
+import TextInput from '../../components/textgroup/text-input';
 
 class MyProfile extends Component {
     constructor(props) {
@@ -10,11 +14,92 @@ class MyProfile extends Component {
     }
     render() { 
         return ( 
-            <View>
-                <Text>My Profile is Hear</Text>
+            <View style={styles.container}>
+                <View style={styles.Header}>
+                    <Text>My Profile is Hear</Text>
+                </View>
+                <View style={styles.form}>
+                    <View style={styles.card}>
+                            <TextInput 
+                                            label="نام و نام فامیل"
+                                            placeholder="امید آرمانی"
+                                            style={styles.txtInput}
+                                            maxLength={30}
+                                            keyboardType={'numeric'}
+                                            />
+                                            
+                        </View>
+                        <View style={styles.card}>
+                            <TextInput 
+                                            label="تلفن همراه"
+                                            placeholder="+98 912 345 6789"
+                                            style={styles.txtInput}
+                                            maxLength={11}
+                                            keyboardType={'numeric'}
+                                            />
+                                            
+                        </View>
+                        <View style={styles.card}>
+                            <TextInput 
+                                            label="پست الکترونیک"
+                                            placeholder="nfo@example.com"
+                                            style={styles.txtInput}
+                                            maxLength={40}
+                                            keyboardType={'numeric'}
+                                            />
+                                            
+                        </View>
+                        <View style={styles.card}>
+                            <TextInput 
+                                            label="استان"
+                                            placeholder="تهران"
+                                            style={styles.txtInput}
+                                            maxLength={20}
+                                            keyboardType={'numeric'}
+                                            />
+                                            
+                        </View>
+
+                </View> 
+                <View style={styles.btnContainer}></View>
             </View>
          );
     }
 }
  
+
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+
+    },
+    Header:{
+        flex:2,
+        backgroundColor: colors.red,
+        justifyContent: 'center',
+        alignItems:'center',
+        
+    },
+    form:{
+        flex:4,
+        backgroundColor:colors.silver,
+        padding:10,
+
+
+    },
+    txtInput:{
+        width:350, 
+        color:colors.black 
+      },    
+      card:{
+        flex:1, 
+        alignContent:'center',
+        alignItems:'center',
+        justifyContent: 'center',
+    },
+    btnContainer:{
+        backgroundColor:colors.black,
+        flex:1,
+    }
+})
 export default MyProfile;
